b39bd0017b034d15e8b6d6504805c7fb7ae40801
MiguelAngelTG7 <michael_angello@hotmail.com>
2024-11-02 19:46:08 -0500

    agregado variable archivos estaticos

--- reservas/settings.py
+++ reservas/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware'
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -141,3 +142,5 @@ AUTH_USER_MODEL = 'gestion.UsuarioModel'
 SIMPLE_JWT = {
     "ACCESS_TOKEN_LIFETIME": timedelta(minutes=60),
 }
+
+STATIC_ROOT=BASE_DIR / 'estaticos'
